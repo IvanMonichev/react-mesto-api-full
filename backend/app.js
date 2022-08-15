bc272dccdf447f91dcc96e600181f71bf1ad2bb1
Ivan Monichev <ivan@monichev.ru>
2022-08-16 01:27:45 +0300

    initial commit

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const { PORT = 3000 } = process.env;
 
 const router = require('./routes');
@@ -12,33 +13,22 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(function(req, res, next) {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
-  const requestHeaders = req.headers['access-control-request-headers'];
-  const allowedCors = [
+
+const options = {
+  origin: [
     'https://monichev.mesto.nomoredomains.sbs',
     'http://monichev.mesto.nomoredomains.sbs',
-    'https://api.monichev.mesto.nomoredomains.sbs',
     'http://api.monichev.mesto.nomoredomains.sbs',
     'http://localhost:3001'
-  ];
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  credentials: true,
+};
 
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-
-    if (method === 'OPTIONS') {
-      // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-      res.header('Access-Control-Allow-Headers', requestHeaders);
-      // завершаем обработку запроса и возвращаем результат клиенту
-      return res.end();
-    }
-  }
-
-  next();
-});
+app.use('*', cors(options));
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
@@ -49,8 +39,8 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.use(requestLogger);
 app.use(router);
-app.use(errorLogger);
 app.use(errors());
+app.use(errorLogger);
 app.use(serverError);
 
 app.listen(PORT, () => {
