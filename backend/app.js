43d7c67ef8811aafec097477d346d148785a34f4
Ivan Monichev <63107880+IvanMonichev@users.noreply.github.com>
2022-08-15 17:55:29 +0300

    initial commit

--- backend/app.js
+++ backend/app.js
@@ -2,17 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
-
+const cors = require('cors');
 
 const { PORT = 3000 } = process.env;
-const allowedCors = [
-  'https://monichev.mesto.nomoredomains.sbs/',
-  'http://monichev.mesto.nomoredomains.sbs/',
-  'https://api.monichev.mesto.nomoredomains.sbs/',
-  'http://api.monichev.mesto.nomoredomains.sbs/',
-  'http://api.monichev.mesto.nomoredomains.sbs/users/me',
-  'localhost:3000'
-];
+
 
 
 const router = require('./routes');
@@ -20,30 +13,26 @@ const serverError = require('./middlewares/serverError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
-
-app.use(function(req, res, next) {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  // проверяем, что источник запроса есть среди разрешённых
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  res.header('Access-Control-Allow-Origin', "*");
-  const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
-  const requestHeaders = req.headers['access-control-request-headers'];
-  if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    // завершаем обработку запроса и возвращаем результат клиенту
-    return res.end();
-  }
-
-  next();
-});
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const options = {
+  origin: [
+    'https://monichev.mesto.nomoredomains.sbs/',
+    'http://monichev.mesto.nomoredomains.sbs/',
+    'https://api.monichev.mesto.nomoredomains.sbs/',
+    'http://api.monichev.mesto.nomoredomains.sbs/',
+    'http://localhost:3000'
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  credentials: true,
+};
+
+app.use('*', cors(options));
+
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
