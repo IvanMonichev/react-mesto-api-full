03d454d4e452a5f66c97dac68bcfd9afd48b635b
Ivan Monichev <ivan@monichev.ru>
2022-08-16 19:57:33 +0300

    feat: add crash test

--- backend/app.js
+++ backend/app.js
@@ -34,6 +34,11 @@ app.use(requestLogger);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 app.use(router);
 app.use(errors());
 app.use(errorLogger);
