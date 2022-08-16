969bbfe65db231e92638955b0c42bf645a217eb6
Ivan Monichev <ivan@monichev.ru>
2022-08-16 21:44:01 +0300

    fix: change error text

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -71,7 +71,7 @@ const likeCard = (request, response, next) => {
   )
     .then((card) => {
       if (!card) {
-        throw new NotFoundError('Переданы некорректные данные для постановки лайк');
+        throw new NotFoundError('Карточки не существует');
       } else {
         response.send(card);
       }
