97a5e98163f6a0f4521c6fa8238d403fa972cec5
Ivan Monichev <ivan@monichev.ru>
2022-08-16 21:50:20 +0300

    hotfix: add default value from jwt

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const { NODE_ENV, JWT_SECRET } = process.env;
+const { NODE_ENV, JWT_SECRET = 'dev-secret' } = process.env;
 
 const getJwtToken = (id) => jwt.sign({ id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
 
